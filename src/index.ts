3a2759931119f4d18f7bdcee6e0949ea6c67e50c
ryanC <ryan149347@gmail.com>
2022-07-15 15:54:12 +0800

    feat: export RoomAllocation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import CustomInputNumber from './components/CustomInputNumber'
+import RoomAllocation from './components/RoomAllocation'
 // styles
 import '@master/css'
 import './index.css'
 
-export { CustomInputNumber }
+export { CustomInputNumber, RoomAllocation }
